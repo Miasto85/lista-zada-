52636bcfe703df31ec30db6a37bee2f948c23c42
Karol Kosior <karol.kosior@gmail.com>
2023-02-23 21:29:13 +0100

    add js

---
+++ js/script.js
@@ -0,0 +1,43 @@
+{
+    const tasks = [];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li
+            $(task.done ? " style=\"text-decoration: line-through\"" : "">
+            ${task.content}
+            </li>
+            `;
+         }
+         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    };
+
+    const init = () => {
+        render();
+
+       const form =document.querySelector(".js-form") 
+       form.addEventListener("submit", (event) => {
+       event.preventDefault();
+
+       const newTaskContent = document.querySelector("js.newTask").ariaValueMax.trim();
+        if (newTaskContent === "") {
+            return;
+        }
+
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+
+
+
+       });
+
+    };
+    init();
+}
