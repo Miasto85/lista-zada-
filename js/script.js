52323d1328f01c393dad6dbb075e5552c0cbfdc9
Karol Kosior <karol.kosior@gmail.com>
2023-03-08 21:38:34 +0100

    add new function

--- js/script.js
+++ js/script.js
@@ -7,36 +7,36 @@
         for (const task of tasks) {
             htmlString += `
             <li
-            $(task.done ? " style=\"text-decoration: line-through\"" : "">
+            ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
             ${task.content}
             </li>
             `;
-         }
-         document.querySelector(".js-tasks").innerHTML = htmlString;
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
 
     };
 
     const init = () => {
         render();
 
-       const form =document.querySelector(".js-form") 
-       form.addEventListener("submit", (event) => {
-       event.preventDefault();
+        const form = document.querySelector(".js-form")
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
 
-       const newTaskContent = document.querySelector("js.newTask").ariaValueMax.trim();
-        if (newTaskContent === "") {
-            return;
-        }
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+            if (newTaskContent === "") {
+                return;
+            }
 
-        tasks.push({
-            content: newTaskContent,
+            tasks.push({
+                content: newTaskContent,
+            });
+
+            render();
+
+
+
         });
-
-        render();
-
-
-
-       });
 
     };
     init();
