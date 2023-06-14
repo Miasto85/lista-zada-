a0b2c0961bd91791921a8b780b9b24afed91daf9
Karol Kosior <karol.kosior@gmail.com>
2023-06-14 21:57:35 +0200

    supplement js after 4 months

--- js/script.js
+++ js/script.js
@@ -1,43 +1,81 @@
 {
     const tasks = [];
 
-    const render = () => {
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    }
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    }
+
+    const toggleTaskDone = (index) => {
+        tasks[index].done = !tasks[index].done;
+        render();
+    }
+    const bindEvents = () => {
+        const removeButton = document.querySelectorAll(".js-remove");
+
+        removeButton.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const toggleDoneButton = document.querySelectorAll(".js-done");
+
+        toggleDoneButton.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+    }
+
+    function onFormSubmit(event) {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
-            <li
-            ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
-            ${task.content}
+            <li class="list__items">
+                <button class="list__button--done js-done">${task.done ? "✓" : ""}</button>
+                <span class="${task.done ? "list__text--done" : ""}"> ${task.content} </span>
+                <button class="list__button--remove js-remove">🗑</button>
             </li>
-            `;
+        `;
         }
+
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
-    };
+        bindEvents();
+    }
 
-    const init = () => {
-        render();
+const onFormSubmit = (event) => {
+    event.preventDefault();
 
-        const form = document.querySelector(".js-form")
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
+    const newTaskContent = document.querySelector(".js-newTask").ariaValueMax.trim();
 
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
-            if (newTaskContent === "") {
-                return;
-            }
+    if (newTaskContent === "") {
+        return;
+    }
 
-            tasks.push({
-                content: newTaskContent,
-            });
+    addNewTask(newTaskContent);
 
-            render();
+    const newTaskInput = document.querySelector(".js-newTask");
+    newTaskInput.value = "";
+    newTaskInput.focus();
 
+};
 
+const init = () => {
+    render();
 
-        });
+    const form = document.querySelector(".js-form");
 
-    };
-    init();
+    form.addEventListener("submit", onFormSubmit);
+};
+init();
 }
